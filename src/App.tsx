eb7e4c3b88161be57a6703032645d08fd7db62c6
Hazem Haddad <Hazem.Haddad@foothillsolutions.com>
2023-01-10 06:54:42 -0800

    To Do list Form Style

--- src/App.tsx
+++ src/App.tsx
@@ -7,11 +7,11 @@ function App() {
 
   const[query,setQuery]=useState<string>('');
   const[toDoList , setToDoList]=useState<ToDoLIst[]>([]);
+  console.log(toDoList);
   return (
     <div className="App">
     <h1>Simple To do list with TS</h1>
     <ToDoList_Form query={query} setQuery={setQuery} toDoList={toDoList} setToDoList={setToDoList}  />
-
     </div>
   );
 }
--- src/components/ToDoList_Form.tsx
+++ src/components/ToDoList_Form.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { ToDoLIst } from '../Types/Types'
+import'./style/toDoList.css'
 
 type Prop = {
     query:string;
@@ -8,7 +9,8 @@ type Prop = {
     setToDoList: React.Dispatch<React.SetStateAction<ToDoLIst[]>>;
 }
 export const ToDoList_Form = ({query , setQuery ,toDoList , setToDoList}:Prop) => {
-const handlAdd =(e:React.FormEvent)=>{
+
+ const handlAdd =(e:React.FormEvent)=>{
  e.preventDefault();
  const toDo:ToDoLIst={
     id: toDoList.length+1,
@@ -18,15 +20,17 @@ const handlAdd =(e:React.FormEvent)=>{
  if(query)
  {
     setToDoList([...toDoList,toDo]);
+    setQuery('');
+    console.log(toDoList);
  }
 }
-console.log(toDoList);
+
   return (
   <form className='tdo_form' onSubmit={handlAdd}>
    <input className='tdo_input' 
    value={query}
    onChange={(e)=>setQuery(e.target.value)}/>
-   <button className='tdo_create_tdl' >Add !</button>
+   <button className='tdo_create_tdl' >Add</button>
   </form>
   )
 }
