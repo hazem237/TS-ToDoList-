b55f83966e2427d5f96b1ce5eea58ebab620d86c
Hazem Haddad <Hazem.Haddad@foothillsolutions.com>
2023-01-10 08:13:52 -0800

    Add Task icons

--- src/components/SingleTask.tsx
+++ src/components/SingleTask.tsx
@@ -1,7 +1,11 @@
 import React from 'react'
 import { ToDoLIst } from '../Types/Types'
-
+import { FaBeer } from 'react-icons/fa';
+import 'react-icons/bs'
 import'./style/toDoList.css'
+import { BsFillTrashFill } from 'react-icons/bs';
+import{MdModeEditOutline, MdOutlineDone} from 'react-icons/md'
+
 type Prop = {
     toDoList:ToDoLIst[];
     setToDoList: React.Dispatch<React.SetStateAction<ToDoLIst[]>>;
@@ -10,9 +14,16 @@ export const SingleTask :React.FC<Prop> = ({toDoList ,setToDoList}:Prop) => {
     console.log(toDoList)
   return (
    <div className='tasks_container'>
-   
+
      {toDoList.map((task)=>(
-       <div className='task_container'>{task.title}</div>
+       <div className='task_container'>
+        {task.title}
+        <span className='task_icon_container'>
+         <BsFillTrashFill/>
+         <MdModeEditOutline/>
+         <MdOutlineDone/>
+        </span>
+       </div>
      ))}
     
    </div>
