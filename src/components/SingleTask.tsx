b4c131406f53edeb82c329a5f444606e7beec917
Hazem Haddad <Hazem.Haddad@foothillsolutions.com>
2023-01-10 08:25:52 -0800

    Enable Delete Task

--- src/components/SingleTask.tsx
+++ src/components/SingleTask.tsx
@@ -10,21 +10,24 @@ type Prop = {
     toDoList:ToDoLIst[];
     setToDoList: React.Dispatch<React.SetStateAction<ToDoLIst[]>>;
 }
+
 export const SingleTask :React.FC<Prop> = ({toDoList ,setToDoList}:Prop) => {
-    console.log(toDoList)
+    const DeleteHandler=(id:number):void=>{
+      setToDoList(toDoList.filter((task)=>(task.id!=id)));
+    }
   return (
    <div className='tasks_container'>
-
-     {toDoList.map((task)=>(
-       <div className='task_container'>
-        {task.title}
+     {toDoList.length>0 ? toDoList.map((task)=>(
+       <div className='task_container' key={task.id}>
+        <span className='task_title'> {task.title}</span>
         <span className='task_icon_container'>
-         <BsFillTrashFill/>
+         <BsFillTrashFill onClick={()=>DeleteHandler(task.id)}/>
          <MdModeEditOutline/>
          <MdOutlineDone/>
         </span>
-       </div>
-     ))}
+       </div> 
+     )): <h1>No Tasks</h1>}
+     
     
    </div>
   )
